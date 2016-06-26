d6edae97fb26e72dff34e327a1015f350454151f
TRVS <tjagapinan@yahoo.com>
2016-06-26 09:03:43 -0700

    Travis.js
    Code used to manage namespacing in js.

---
+++ Home/travis.js
@@ -0,0 +1,37 @@
+﻿var travis = {
+    utilities: {}
+    , layout: {}
+    , page: {
+        handlers: {
+        }
+        , startUp: null
+    }
+    , services: {}
+    , ui: {}
+
+};
+travis.moduleOptions = {
+    APPNAME: "SabioApp"
+        , extraModuleDependencies: []
+        , runners: []
+        , page: travis.page//we need this object here for later use
+}
+
+
+travis.layout.startUp = function () {
+
+    console.debug("travis.layout.startUp");
+
+    //this does a null check on sabio.page.startUp
+    if (travis.page.startUp) {
+        console.debug("travis.page.startUp");
+        travis.page.startUp();
+    }
+
+};
+travis.APPNAME = "App";//legacy
+
+
+
+
+$(document).ready(travis.layout.startUp);
